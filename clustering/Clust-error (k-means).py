a30c3ff9a4d372d365852473d52b474da5532229
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2026-04-07 12:18:20 +0300

    elbow method

---
+++ clustering/Clust-error (k-means).py
@@ -0,0 +1,40 @@
+import csv
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+data = pd.read_csv('../datasets/VDS_s.csv')
+
+error = []
+tmper = []
+N = range(2, 10)    # количество кластеров
+x = 5  # количество повторных циклов
+for i in range(x):     # цикл для вычисления средней ошибки для конкретного кол-ва кластеров
+    tmper = []
+    data = data.sample(frac=1)  # перетасовка
+    for n in N:
+        # модель кластеризации и вычисление ошибки
+        clust_model = KMeans(n_clusters=n, random_state=None, n_init='auto')
+        clust_model.fit(data.iloc[:, 4:])
+        #tmper.append(silhouette_score(data.iloc[:, 5:], clust_model.labels_, metric='euclidean'))
+        tmper.append(clust_model.inertia_)
+
+    for j, m in enumerate(tmper):
+        if i == 0:
+            error.append(m)
+        else:
+            error[j]+=m
+
+    print("Итерация №: ", i)
+
+for i in range(len(error)):
+    error[i] = error[i] / x
+
+plt.plot(N, error)
+plt.xlabel("Number of clusters")
+plt.ylabel("Sum of Squared Errors")
+plt.show()
