89d30530966b76b09bf1a4c89f8b8be730952e60
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2026-05-04 16:10:53 +0300

    Update DatasetPrep 4AI.py
    upd. normbymax

--- datasets/DatasetPrep 4AI.py
+++ datasets/DatasetPrep 4AI.py
@@ -6,28 +6,21 @@ import pandas as pd
 from sympy import false
 import copy
 import scipy.stats as sts
+import seaborn as sns
 
 # нормализация знечений признаков от 0 до 1 (с сохранением файла с нормализаторами (макс.))
-def normbymax(trainset):
-    tmpp = []
-    for k in range(3, len(trainset[0])):
-        maxi = trainset[0][k]
-        for i in range(len(trainset)):
-            if (maxi < trainset[i][k]):
-                maxi = trainset[i][k]
+def normbymax(rawdata, cols):
 
-        tmpp.append(maxi)
+    maxi = []
+    for a in cols:
+        maxi.append(rawdata[a].max())
+        rawdata[a] = rawdata[a] / rawdata[a].max()
 
-        for j in range(len(trainset)):
-            trainset[j][k] = trainset[j][k] / maxi
+    maxi = np.array(maxi)
+    maxi = pd.DataFrame([maxi], columns=cols)
+    maxi.to_csv("fornorm VDS_r (ITcosts_r) 0 prop IQR.csv", index=False)
 
-    features = ['factoriescap_s', 'ITcosts_s', 'skvozcosts_s', 'trainingcosts_s']
-
-    tmpp = np.array(tmpp)
-    tmpp = pd.DataFrame([tmpp], columns=features)
-    tmpp.to_csv("fornorm factoriescap_s (costs) 0 prop visnorm.csv", index=False)
-
-    return trainset
+    return rawdata
 
 # умножить рублевые признаки на соответствующую долю инфляции
 def normbyinf(trainset, rubfeatures):
@@ -52,57 +45,81 @@ def remove_outliers(data: pd.DataFrame) -> pd.DataFrame:
     data = copy.deepcopy(data)
 
     for col in data.columns:
-        if col != 'sector' and col != 'year' and col != 'okved2':
+        if col != 'region' and col != 'year' and col != 'okato':
             data = data[(data[col] < np.quantile(data[col], 0.75) + 4 * sts.iqr(data[col])) &
                         (data[col] > np.quantile(data[col], 0.25) - 4 * sts.iqr(data[col]))]
 
     return data
 
+# Анализ зависимости отдельных признаков от определяющего
+def visanalysis(rawdata):
+    rawdata = rawdata.sort_values(by=['VDS_r']).reset_index(drop=True)
+    #rawdata = rawdata[:int(len(rawdata) / 2)]
 
-features = ['factoriescap_s', 'ITcosts_s', 'skvozcosts_s', 'trainingcosts_s']
+    #rawdata['skvozcosts_r'] = rawdata['skvozcosts_r'] / rawdata['ITcosts_r']
+    rawdata['ITcosts_r'] = rawdata['ITcosts_r'] / rawdata['VDS_r']
+
+    rawdata['VDS_r'] = rawdata['VDS_r'] / rawdata['VDS_r'].max()
+    rawdata['ITcosts_r'] = rawdata['ITcosts_r'] / rawdata['ITcosts_r'].max()
+    #rawdata['skvozcosts_r'] = rawdata['skvozcosts_r'] / rawdata['skvozcosts_r'].max()
+
+    plt.scatter(rawdata['VDS_r'], rawdata['ITcosts_r'], marker='o')
+    # plt.plot(rawdata['factoriescap_s'])
+    # plt.plot(rawdata['skvozcosts_s'])
+    # plt.plot([0, 1], [0, 1], ls='--', c='red', label='Linear')
+    plt.show()
+
+# Чистка датасета от выбросов (определенных визуально)
+def visnorm(rawdata):
+    # на основе визуальной нормализации
+    rawdata = rawdata[rawdata['okved2'] != 'C']
+    #rawdata = rawdata[(rawdata['okved2'] != '19') | (rawdata['year'] != 2022)]
+    awdata = rawdata[(rawdata['okved2'] != '19') | (rawdata['year'] != 2023)]
+
+    #rawdata = rawdata[(rawdata['okved2'] != '19.2') | (rawdata['year'] != 2022)]
+    #rawdata = rawdata[(rawdata['okved2'] != '19.2') | (rawdata['year'] != 2023)]
+
+    #rawdata = rawdata[(rawdata['okved2'] != '08') | (rawdata['year'] != 2021)]
+    rawdata = rawdata[(rawdata['okved2'] != 'B') | (rawdata['year'] != 2021)]
+
+features = ['VDS_r', 'ITcosts_r', 'skvozcosts_r', 'trainingcosts_r']
 
 # признаки для ценового нормирования
 allrubfeatures = ['VDS_s', 'AIcosts_s' 'ITcosts_s', 'skvozcosts_s', 'trainingcosts_s',
                   'RDcosts_s', 'RDsalary_s', 'RDequip_s', 'factoriescap_s']
 
+allrubfeatures_r = ['VDS_r', 'AIcosts_r' 'ITcosts_r', 'skvozcosts_r', 'trainingcosts_r',
+                  'RDcosts_r', 'RDsalary_r', 'RDequip_r', 'factoriescap_r']
+
 # получение и сортировка данных
-rawdata = pd.read_csv("../data/factoriescap_s.csv")
-rawdata = rawdata.sort_values(by=['okved2', 'year'])
+rawdata = pd.read_csv("../data/VDS_r.csv", dtype={'okato': str})
+rawdata = rawdata.sort_values(by=['okato', 'year'])
 
 tempset = []
 for k in range(1, len(features)):
-    tempset = pd.read_csv('../data/'+features[k]+'.csv')
-    tempset = tempset[tempset.columns.drop('okved2')]
-    rawdata = rawdata.merge(tempset, on=['sector', 'year'], how='left')
+    tempset = pd.read_csv('../data/'+features[k]+'.csv', dtype={'okato': str})
+    tempset = tempset[tempset.columns.drop('region')]
+    rawdata = rawdata.merge(tempset, on=['okato', 'year'], how='left')
 
 rawdata = rawdata.dropna()
 
+
+minyear = rawdata['year'].min()
 rawdata = normbyinf(rawdata, features)
 
-#rawdata = remove_outliers(rawdata)
+#visanalysis(rawdata)
 
-# на основе визуальной нормализации
-rawdata = rawdata[rawdata['okved2'] != 'C']
-rawdata = rawdata[(rawdata['okved2'] != '19') | (rawdata['year'] != 2022)]
-rawdata = rawdata[(rawdata['okved2'] != '19') | (rawdata['year'] != 2023)]
+rawdata['skvozcosts_r'] = rawdata['skvozcosts_r'] / rawdata['ITcosts_r']
+rawdata['trainingcosts_r'] = rawdata['trainingcosts_r'] / rawdata['ITcosts_r']
+rawdata['ITcosts_r'] = rawdata['ITcosts_r'] / rawdata['VDS_r']
 
-rawdata = rawdata[(rawdata['okved2'] != '19.2') | (rawdata['year'] != 2022)]
-rawdata = rawdata[(rawdata['okved2'] != '19.2') | (rawdata['year'] != 2023)]
+rawdata = remove_outliers(rawdata)
 
+rawdata = normbymax(rawdata, features)
 
-rawdata = rawdata[(rawdata['okved2'] != '08') | (rawdata['year'] != 2021)]
-rawdata = rawdata[(rawdata['okved2'] != 'B') | (rawdata['year'] != 2021)]
+#features = ['sector', 'okved2', 'year', 'VDS_r', 'ITcosts_r', 'skvozcosts_r', 'trainingcosts_r']
 
-rawdata['skvozcosts_s'] = rawdata['skvozcosts_s'] / rawdata['ITcosts_s']
-rawdata['trainingcosts_s'] = rawdata['trainingcosts_s'] / rawdata['ITcosts_s']
-rawdata['ITcosts_s'] = rawdata['ITcosts_s'] / rawdata['factoriescap_s']
-
-rawdata = np.array(rawdata)
-rawdata = normbymax(rawdata)
-
-features = ['sector', 'okved2', 'year', 'factoriescap_s', 'ITcosts_s', 'skvozcosts_s', 'trainingcosts_s']
-
-rawdata = pd.DataFrame(rawdata, columns=features)
-rawdata.to_csv('factoriescap_s (costs) 0 prop visnorm.csv', index=False)
+#rawdata = pd.DataFrame(rawdata, columns=features)
+rawdata.to_csv('VDS_r (ITcosts_r) 0 prop IQR.csv', index=False)
 
 print('done')
