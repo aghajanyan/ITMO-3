895527d8c21654fb8f76e4e89230fd78310867e0
Albert Aghajanyan <49070457+aghajanyan@users.noreply.github.com>
2026-08-21 11:15:41 +0300

    Potential AI impact v2.0

--- clustering/sepsectors/impact_calc.py
+++ clustering/sepsectors/impact_calc.py
@@ -1,35 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
 
-def on_click():
-    val = slider.get()
-    com = combo.get()
+significance = []   #коэффициенты экономической значимости
+task = []    #выбор класса задач
 
-    minimp = (val * impact[com]['min'] * capability[com]['min']) * 100
-    maximp = (val * impact[com]['max'] * capability[com]['max']) * 100
+# сохранение набора задач
+def on_click_save():
+    significance.append(slider.get())
+    slider.set(0)
+    task.append(combo.get())
+    combo.set("Оптимизация")
+
+# вычислить потенциальную выгоду
+def on_click_calc():
+    if slider.get() != 0:
+        significance.append(slider.get())
+        task.append(combo.get())
+
+    minimp = 0
+    maximp = 0
+    for i in range(len(task)):
+        minimp += (significance[i] * impact[task[i]]['min'] * capability[task[i]]['min']) * 100
+        maximp += (significance[i] * impact[task[i]]['max'] * capability[task[i]]['max']) * 100
 
     label3.config(text=f"Потенциальный рост ВДС от {minimp:.1f}% до {maximp:.1f}%")
 
+# усредненные выгоды от использования МЛ/ИИ в сравнении с классическими подходами
 impact = {
     "Оптимизация": {'min': 0.1, 'max': 0.3},
     "Прогнозирование": {'min': 0.1, 'max': 0.3},
-    "Распознование": {'min': 0.3, 'max': 0.9},
-    "Логистика": {'min': 0.05, 'max': 0.25},
-    "Комбинаторный поиск": {'min': 0.4, 'max': 1}
+    "Распознование": {'min': 0.5, 'max': 0.9},
+    "Логистика": {'min': 0.05, 'max': 0.15},
+    "Комбинаторный поиск": {'min': 0.05, 'max': 0.1}
 }
 
+# сложность/вероятность успешной реализации (пока вилка из 0.3 и 0.7)
 capability = {
-    "Оптимизация": {'min': 0.4, 'max': 0.7},
-    "Прогнозирование": {'min': 0.5, 'max': 0.8},
-    "Распознование": {'min': 0.7, 'max': 0.9},
-    "Логистика": {'min': 0.5, 'max': 0.7},
-    "Комбинаторный поиск": {'min': 0.15, 'max': 0.3}
+    "Оптимизация": {'min': 0.3, 'max': 0.7},
+    "Прогнозирование": {'min': 0.3, 'max': 0.7},
+    "Распознование": {'min': 0.3, 'max': 0.7},
+    "Логистика": {'min': 0.3, 'max': 0.7},
+    "Комбинаторный поиск": {'min': 0.3, 'max': 0.7}
 }
 
 # Создаем главное окно
 root = tk.Tk()
-root.title("Potential AI impact")
-root.geometry("600x250")
+root.title("Potential AI impact v2.0")
+root.geometry("600x300")
 
 label1 = tk.Label(root, text="Категория задачи", font=("Arial", 10))
 label1.pack()
@@ -52,8 +69,11 @@ slider = tk.Scale(
 )
 slider.pack(pady=(0,20))
 
-button = tk.Button(root, text="Вычислить потенциальный вклад", command=on_click, font=("Arial", 10, "bold"))
-button.pack(pady=(0,20))
+button1 = tk.Button(root, text="Добавить еще одну задачу?", command=on_click_save, font=("Arial", 10, "bold"))
+button1.pack(pady=(0,20))
+
+button2 = tk.Button(root, text="Вычислить потенциальный вклад", command=on_click_calc, font=("Arial", 10, "bold"))
+button2.pack(pady=(0,20))
 
 label3 = tk.Label(root, text="", fg='green', font=("Arial", 12, "bold"))
 label3.pack()
